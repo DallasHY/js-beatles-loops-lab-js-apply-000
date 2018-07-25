c7d457a7da8eb74cd9400fa5feb6b73f1c72569b
hyde.dallas1@gmail.com <hyde.dallas1@gmail.com>
2018-07-25 03:45:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // add solution here
 
-function theBeatlesPlay(musicians, instruments) {
-  var arr = [];
+function theBeatlesPlay(musicians, instruments){
+    var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+   return array;
 }
